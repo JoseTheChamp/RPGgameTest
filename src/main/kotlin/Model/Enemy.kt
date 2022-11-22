5039477b760334d30b0a9e10e52cfd41a6acc51a
JoseTheChamp <apep.knotek@gmail.com>
2022-11-22 15:54:40 +0100

    FightEvent - without fight mech

---
+++ src/main/kotlin/Model/Enemy.kt
@@ -0,0 +1,4 @@
+package Model
+
+class Enemy : Character() {
+}
--- src/main/kotlin/Model/FightEvent.kt
+++ src/main/kotlin/Model/FightEvent.kt
@@ -1,4 +1,23 @@
 package Model
 
-class FightEvent {
+class FightEvent (override var id : Int, override var name : String, override var story : String, var enemy: Enemy, var optionWon : EventOption, var optionLost : EventOption): Event() {
+
+   fun vypis(hero: Hero) : EventOption{
+       println("------------------------------")
+       println(id.toString() + name + " - " + story)
+       println("Stoji před tebou " + enemy.name)
+       // TODO - boj
+            if (true){
+                println("Won")
+                return optionWon
+            }
+       println("Lost")
+       return optionLost
+   }
+
+    override fun run(hero: Hero): Int {
+        var option = vypis(hero)
+        readln()
+        return option.targetId;
+    }
 }
